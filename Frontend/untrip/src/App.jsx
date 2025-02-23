d421663dd90f4f119abc5197700f5df5e5d00d67
YASAR300 <everythingdecoding@gmail.com>
2025-02-23 23:42:40 +0530

    add secure booking page route to the application

--- Frontend/untrip/src/App.jsx
+++ Frontend/untrip/src/App.jsx
@@ -5,7 +5,8 @@ import Sale from './components/sale';
 import Hotel from './components/HotelList';
 import Destination from './components/Destination';
 import Resort from './components/resort';
-import Footer from './components/Footer';  
+import Footer from './components/Footer'; 
+import Secure from './components/SecureBooking';  
 import Page from './Pages/Page';
 import HotelListing from './Pages/HotelListing';
 import SignIn from './Userauth/Signin';
@@ -42,6 +43,7 @@ function MainContent() {
         </>} />
         <Route path="/hotel/:id" element={<Page />} />
         <Route path="/HotelDetails/:id" element={<HotelListing />} />
+        <Route path="/secure" element={<Secure />} />
       </Routes>
       {!isSignInPage && <Footer />}
     </>
